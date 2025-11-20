8f1ae1fd1b8359bdd392669a60e702bdb9685688
shashankbishtgehu <shashankbishtgehu@gmail.com>
2025-11-20 11:48:56 +0530

    new changes

---
+++ Day-1/course/task.py
@@ -0,0 +1,9 @@
+# Write your code below this line 👇
+print("Hello world!")
+print("Hello\nworld!")
+print("Hello" + " " + "world!")
+input("What is your name?")
+print("Hello " + input("What is your name?")+"!")
+greeting = "hello"
+print(greeting + "python")
+print("Characters in you name is: " + str(len(input("What is your name?"))))
