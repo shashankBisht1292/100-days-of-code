f1c0182889a51b4f0c0e245551cf13ea8bd0e7d7
shashankbishtgehu <shashankbishtgehu@gmail.com>
2025-11-20 11:14:44 +0530

    Day 1 - Brand Name Generator

---
+++ task.py
@@ -0,0 +1,14 @@
+# Write your code below this line 👇
+print("Hello world!")
+print("Hello\nworld!")
+print("Hello" + " " + "world!")
+input("What is your name?")
+print("Hello " + input("What is your name?")+"!")
+greeting = "hello"
+print(greeting + "python")
+print("Characters in you name is: " + str(len(input("What is your name?"))))
+
+print("Welcome to the Brand Name Generator.")
+cityName = input("What's the name of the city you grew up in?\n")
+petName = input("What's your pet's name?\n")
+print("Your brand name could be: " + cityName + " " + petName)
